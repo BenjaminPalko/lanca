11a7823a7db08200e7bc0d1fbae3e0925fb127f5
Benjamin <benjaminpalko@hotmail.com>
2023-05-22 14:24:42 -0400

    add loader visual

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,7 +5,13 @@ const Loader = function ({element}: { element: ReactElement }) {
     const Fallback = function () {
 
         return (
-            <></>
+            <div className={'h-full w-full flex justify-center items-center antialiased'}>
+                <span className="relative flex h-24 w-24">
+                    <span
+                        className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+                    <span className="relative inline-flex rounded-full h-24 w-24 bg-red-500"></span>
+                </span>
+            </div>
         )
     }
 
