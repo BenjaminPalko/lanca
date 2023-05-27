3cda699e9e5de86b59a77f36002aab97e0e0a8bf
Benjamin <benjaminpalko@hotmail.com>
2023-05-27 10:34:03 -0400

    person card better

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -15,39 +15,44 @@ const PersonCard = function ({person}: PersonCardProps) {
     }
 
     return (
-        <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg hover:cursor-pointer max-w-sm"
-                 onClick={handleClick}>
+        <a onClick={handleClick}
+           className="group relative block bg-black overflow-hidden rounded-lg shadow transition hover:shadow-lg hover:cursor-pointer">
             <img
-                alt="Office"
+                alt="Developer"
                 src={person.photo.url}
-                className="h-80 w-full object-cover"
+                className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
             />
 
-            <div className="bg-white p-4 sm:p-6">
-
-                <a>
-                    <h3 className="mt-0.5 text-lg text-gray-900">
-                        {person.name}
-                    </h3>
-                </a>
-
-                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                    {
-                        person.about.slice(0, 120).trim() + '...'
-                    }
+            <div className="relative p-4 sm:p-6 lg:p-8">
+                <p className="text-sm font-medium uppercase tracking-widest text-red-600">
+                    Developer
                 </p>
 
-                <ul className={'mt-2 flex flex-wrap gap-2'}>
-                    {
-                        person.skills.map(skill => (
-                            <li key={skill.skill.id}>
-                                <Badge value={skill.skill.name} color={'red'}/>
-                            </li>
-                        ))
-                    }
-                </ul>
+                <p className="text-xl font-bold text-white sm:text-2xl capitalize">{person.name}</p>
+
+                <div className="mt-32 sm:mt-48 lg:mt-64">
+                    <div
+                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+                    >
+                        <p className="text-sm text-white">
+                            {
+                                person.about.slice(0, 120).trim() + '...'
+                            }
+                        </p>
+                        <ul className={'mt-2 flex flex-wrap gap-2'}>
+                            {
+                                person.skills.map(skill => (
+                                    <li key={skill.skill.id}>
+                                        <Badge value={skill.skill.name} color={'red'}/>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </article>
+        </a>
+
     )
 }
 
